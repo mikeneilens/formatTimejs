ae7a0c582c035125faa33ce8a5a4718cfe16d10d
mikeneilens <mike.neilens@gmail.com>
2019-03-17 15:19:39 +0000

    Refactored to use enum and class for times

--- formatTime.js
+++ formatTime.js
@@ -1,34 +1,24 @@
-const unitTimes =[{unit:"year",value:31536000}, {unit:"day",value:86400}, {unit:"hour",value:3600 }, {unit:"minute",value:60},{unit:"second",value:1}];
+//Javascript enum
+const Unit = Object.freeze({
+  second: { secondsPerUnit:1, maxValue:60, text:"second"},
+  minute: { secondsPerUnit:60, maxValue:60, text:"minute"},
+  hour: { secondsPerUnit:3600, maxValue:24, text:"hour"},
+  day: { secondsPerUnit:86400, maxValue:365, text:"day"},
+  year: { secondsPerUnit:31536000, maxValue:100, text:"year"}
+});
 
-function formatIntoText(aTime) {
-    switch (aTime.length) {
-        case 0: 
-          return "none"; break;
-        case 1: 
-          return `${aTime[0].value} ${aTime[0].unit}`;
-        case 2: 
-          return `${aTime[0].value} ${aTime[0].unit} and ${aTime[1].value} ${aTime[1].unit}`;
-        case 3: 
-          return `${aTime[0].value} ${aTime[0].unit}, ${aTime[1].value} ${aTime[1].unit} and ${aTime[2].value} ${aTime[2].unit}`;
-        case 4: 
-          return `${aTime[0].value} ${aTime[0].unit}, ${aTime[1].value} ${aTime[1].unit}, ${aTime[2].value} ${aTime[2].unit} and ${aTime[3].value} ${aTime[3].unit}`;
-        case 5: 
-          return `${aTime[0].value} ${aTime[0].unit}, ${aTime[1].value} ${aTime[1].unit}, ${aTime[2].value} ${aTime[2].unit}, ${aTime[3].value} ${aTime[3].unit} and ${aTime[4].value} ${aTime[4].unit}`; 
+class UnitTime {
+  constructor(unit, quantity) {
+    this.unit = unit;
+    this.value = quantity;
+  }
+  text() {
+    if (this.value > 1) {
+      return `${this.value} ${this.unit.text}s`;
+    } else {
+      return `${this.value} ${this.unit.text}`;
     }
-}
-
-function calcTime(timeInSeconds) {
-
-    let timeRemaining = timeInSeconds;
-
-    function calcTimePerUnit(unitTime) {
-        const newValue = Math.floor(timeRemaining/unitTime.value); 
-        const newUnit = newValue > 1 ? unitTime.unit+ "s" : unitTime.unit;
-        timeRemaining = timeRemaining % unitTime.value;
-        return {unit:newUnit, value:newValue};
-    }
-
-    return unitTimes.map(calcTimePerUnit);
+  }
 }
 
 function formatTime(timeInSeconds) {
@@ -39,6 +29,34 @@ function formatTime(timeInSeconds) {
 
     const calcualtedUnitTimes = calcTime(timeInSeconds).filter(isNonZeroTime);
     return formatIntoText(calcualtedUnitTimes);
+}
+
+const units = [Unit.year, Unit.day, Unit.hour, Unit.minute, Unit.second];
+
+function calcTime(timeInSeconds) {
+
+  function calcTimePerUnit(unit) {
+    const newValue = Math.floor(timeInSeconds/unit.secondsPerUnit) % unit.maxValue; 
+    return new UnitTime(unit, newValue);
+  }
+  return units.map(calcTimePerUnit);
+}
+
+function formatIntoText(aTime) {
+    switch (aTime.length) {
+        case 0: 
+          return "none"; break;
+        case 1: 
+          return `${aTime[0].text()}`;
+        case 2: 
+          return `${aTime[0].text()} and ${aTime[1].text()}`;
+        case 3: 
+          return `${aTime[0].text()}, ${aTime[1].text()} and ${aTime[2].text()}`;
+        case 4: 
+          return `${aTime[0].text()}, ${aTime[1].text()}, ${aTime[2].text()}, and ${aTime[3].text()}`;
+        case 5: 
+          return `${aTime[0].text()}, ${aTime[1].text()}, ${aTime[2].text()}, ${aTime[3].text()} and ${aTime[4].text()}`; 
+    }
 }
 
 console.log(formatTime(0));
